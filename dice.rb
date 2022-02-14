a0520149c449b8af82a0befd135801793f59b5b9
Name <mazyrov03@mail.ru>
2022-02-14 10:22:32 +0900

    Initial commit

--- dice.rb
+++ dice.rb
@@ -2,7 +2,10 @@
 puts "Сколько кинуть кубиков" 
 number = gets.to_i # ввод из консоли число кубиков которые кинуть
 # цикл бросков кубиков введённое количество раз
+num = []
 
+sum = 0
 number.times do
-    print puts "#{rand(6) + 1}\r"
+    print puts "#{rand(6) + 1}\r" 
 end
+puts num
